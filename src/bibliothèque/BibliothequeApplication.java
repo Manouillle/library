3807117ab41a6593463dd56d475c5584592bab73
Manouillle <72972955+Manouillle@users.noreply.github.com>
2020-12-16 10:09:23 +0100

    init project

---
+++ src/bibliothèque/BibliothequeApplication.java
@@ -0,0 +1,10 @@
+package bibliothèque;
+
+public class BibliothequeApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/Livre.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Livre {
+	private String titre;
+	private String nomEmprunteur;
+	private LocalDate dateParution;
+	private LocalDate dateEmprunt;
+	private LocalDate dateRetour;
+}
